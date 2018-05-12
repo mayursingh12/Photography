a4b0e16349b20388b7209fe247316b4c9e3d4ed5
Mayur <mayursingh1220@gmail.com>
2018-05-12 20:47:14 +0530

    theme change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require ./vendors/bootstrap
-//= require ./vendors/mdrz2.8.3.min
-//= require ./vendors/custom.min
-//= require ./vendors/gmap3
-//= require ./vendors/jquery.ajaxchimp.min
-//= require ./vendors/wow.min
+// require jquery
+// require jquery_ujs
+// require turbolinks
+// require ./vendors/bootstrap
+// require ./vendors/mdrz2.8.3.min
+// require ./vendors/custom.min
+// require ./vendors/gmap3
+// require ./vendors/jquery.ajaxchimp.min
+// require ./vendors/wow.min
